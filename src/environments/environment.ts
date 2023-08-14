0e27f07990f3d517dd9f7e758af2d3823d495876
Santiago Vargas N <sanvargas02@hotmail.com>
2023-08-14 11:13:16 -0500

    Métodos de Storage completos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'my-storage-app-dc545',
+    appId: '1:308391645374:web:528fdf1455a0cb5fda0eef',
+    storageBucket: 'my-storage-app-dc545.appspot.com',
+    apiKey: 'AIzaSyDF-A0NaP1X_T0_KaC1e8mnkBnV0a3hGF4',
+    authDomain: 'my-storage-app-dc545.firebaseapp.com',
+    messagingSenderId: '308391645374',
+  },};
